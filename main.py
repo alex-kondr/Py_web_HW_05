53d3ec660306d9a32a43efeca95acbadef2d5408
Oleksandr Kondratiuk <alex_kondr@outlook.com>
2023-01-30 16:54:37 +0200

    save

--- main.py
+++ main.py
@@ -1,37 +1,61 @@
 import aiohttp
 import asyncio
 import argparse
+from datetime import datetime, timedelta
+
+
+
 
 
 parser = argparse.ArgumentParser(
     prog="ExchangeRate",
     description="Exchange rate for EUR to UAH and USD to UAH")
 
-parser.add_argument("amount_of_days", metavar="n", type=int)
+parser.add_argument("n", metavar="amount_of_days", type=int)
 
 args = parser.parse_args()
-print(args.amount_of_days)
+# print(args.n)
 
-quit()
+# quit()
 
 URL = "https://api.privatbank.ua/p24api/exchange_rates"
 
+# async def 
 
-async def main():
+
+async def get_exchange_rates(url: str, date: str):
+    
+    
     
     params = {
         "json": "",
-        "coursid": "5"}
-
+        "coursid": date}
+    
     async with aiohttp.ClientSession() as session:
-        async with session.get("https://api.privatbank.ua/p24api/pubinfo", params=params) as response:
+            async with session.get(url, params=params) as response:
 
-            # print("Status:", response.status)
-            # print("Content-type:", response.headers['content-type'])
-            # print('Cookies: ', response.cookies)
-            # print(response.ok)
-            result = await response.json()
-            return result
+                # print("Status:", response.status)
+                # print("Content-type:", response.headers['content-type'])
+                # print('Cookies: ', response.cookies)
+                # print(response.ok)
+                result = await response.json()
+                return result
+
+async def main():
+    
+    date_now = datetime.now()
+    
+    for _ in range(args.n):
+        
+        delta = timedelta(days=1)        
+        date = date_now - delta
+        date_str = date.strftime("%d.%m.%Y")
+        
+    
+        get_exchange_rates(URL, date_str)
+    
+
+    
 
 
 if __name__ == "__main__":
