cc1e97096bd6a62ac020fe72e1f6f4f387360d3f
alex_kondr <alex_kondr@outlook.com>
2023-01-31 10:26:25 +0200

    save

--- main.py
+++ main.py
@@ -2,32 +2,26 @@ import aiohttp
 import asyncio
 import argparse
 from datetime import datetime, timedelta
+import time
 
-
-
+# time_start = time.time()
 
 
 parser = argparse.ArgumentParser(
     prog="ExchangeRate",
     description="Exchange rate for EUR to UAH and USD to UAH")
 
-parser.add_argument("n", metavar="amount_of_days", type=int)
-
+parser.add_argument("n", metavar="amount_of_days", type=int, choices=range(1, 11))
 args = parser.parse_args()
-# print(args.n)
-
-# quit()
 
 URL = "https://api.privatbank.ua/p24api/exchange_rates"
-
-# async def 
 
 
 async def get_exchange_rates(url: str, date: str):
     
     params = {
         "json": "",
-        "coursid": date}
+        "date": date}
     
     async with aiohttp.ClientSession() as session:
             async with session.get(url, params=params) as response:
@@ -36,34 +30,37 @@ async def get_exchange_rates(url: str, date: str):
                 # print("Content-type:", response.headers['content-type'])
                 # print('Cookies: ', response.cookies)
                 # print(response.ok)
-                result = await response.json()
+                exchanges = await response.json()
                 
-                print(f"{result=}")
-                
-                return result
-
+                # print(f"{date=}, time={time.time() - time_start}")
+    
+    return [exchange for exchange in exchanges["exchangeRate"]
+        if exchange["currency"] == "EUR" or exchange["currency"] == "USD"]
+    
+    
 async def main():
     
-    date_now = datetime.now()
+    date_now = datetime.now()    
+    futures = []
     
-    for _ in range(args.n):
+    for i in range(args.n):
         
-        delta = timedelta(days=1)        
+        delta = timedelta(days=i)        
         date = date_now - delta
         date_str = date.strftime("%d.%m.%Y")
-        
-        print(f"{date_str=}")
-    
-        result = await get_exchange_rates(URL, date_str)
+        futures.append(get_exchange_rates(URL, date_str))
        
-    return result
-    
-
-    
+    return await asyncio.gather(*futures)
 
 
 if __name__ == "__main__":
-    # if platform.system() == 'Windows':
-    #     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    r = asyncio.run(main())
-    print(r)
+    # time_now = time.time()
+    results = asyncio.run(main())
+    
+    # print(time.time() - time_now)
+    
+    for result in results:
+        print(result)
+        
+    # print(time.time() - time_now)
+        
